d7845b8cdf03e3c889bba04758075b33ea171bdd
Sue <codesue@users.noreply.github.com>
2017-05-12 22:58:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, Markup, session, flash
+from flask import Flask, request, render_template, redirect, url_for, Markup
 from newspaper import Article
 from xml.etree  import ElementTree
 from copy import deepcopy
